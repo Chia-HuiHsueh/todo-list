481fa44718fc7bb820826554e63266e063d2727d
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-01-22 22:29:07 +0800

    feat: add todo seeder

--- models/todo.js
+++ models/todo.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
 //schema資料庫綱要
 const Schema = mongoose.Schema
+//把想要的資料結構當成參數傳給 new Schema()
 const todoSchema = new Schema({
   name: {
     type: String, // 資料型別是字串
     required: true // 這是個必填欄位
   }
 })
+//透過 module.exports 把這個 schema 輸出
 module.exports = mongoose.model('Todo', todoSchema)
