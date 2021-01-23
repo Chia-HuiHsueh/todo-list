769449cf4c8f7cebbebc36d1710ef0a677d483ef
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-01-23 16:35:38 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
 const Todo = require('./models/todo')
 
 const app = express()
-const port = 3000
 
-//新增了一個叫 hbs 的樣板引擎，並傳入exphbs相關的參數
-//extname: '.hbs'->指定副檔名為 .hbs
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
+const port = 3000
 
 // 設定連線到 mongoDB
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -28,6 +26,15 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongodb connected!')
 })
+
+//新增了一個叫 hbs 的樣板引擎，並傳入exphbs相關的參數
+//extname: '.hbs'->指定副檔名為 .hbs
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // 設定路由
 // Todo 首頁
 app.get('/', (req, res) => {
@@ -35,6 +42,26 @@ app.get('/', (req, res) => {
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列//撈資料以後想用 res.render()，要先用 .lean() 來處理
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
+})
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+app.post('/todos', (req, res) => {
+
+  //方法一：直接操作資料庫
+  const name = req.body.name    // 從 req.body 拿出表單裡的 name 資料   
+  return Todo.create({ name })     // 呼叫 Todo物件直接新增資料並存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
+
+  //方法二：先產生物件實例,再把實例存入Todo
+
+  // const name = req.body.name 
+  // const todo = new Todo({name})//從Todo產生一個實例
+  // return todo.save()//將實例存入資料庫
+  // .then(()=>res.redirect('/'))
+  // .catch(error => console.log(error))
+
 })
 
 app.listen(port, () => {
