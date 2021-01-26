4598a1746464e322f0a8dfe93576c4f5e6da2224
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-01-26 14:08:28 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,33 +1,18 @@
 const express = require('express')
-const mongoose = require('mongoose')
+
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 
 const routes = require('./routes')
+//對 app.js 而言，Mongoose 連線設定只需要「被執行」，不需要接到任何回傳參數繼續利用，所以這裡不需要再設定變數。
+require('./config/mongoose')
 
 const app = express()
 
 const port = 3000
 
-// 設定連線到 mongoDB
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-
-// 連線異常
-//db.on()：在這裡用 on 註冊一個事件監聽器，用來監聽 error 事件有沒有發生
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-// 連線成功
-//db.once() - 針對「連線成功」的 open 情況，我們也註冊了一個事件監聽器，連線成功只會發生一次，所以這裡特地使用 once，由 once 設定的監聽器是一次性的，一旦連線成功，在執行 callback 以後就會解除監聽器。
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 //新增了一個叫 hbs 的樣板引擎，並傳入exphbs相關的參數
 //extname: '.hbs'->指定副檔名為 .hbs
@@ -42,10 +27,6 @@ app.use(methodOverride('_method'))
 
 // 將 request 導入路由器
 app.use(routes)
-
-
-
-
 
 app.listen(port, () => {
   console.log('App is running on http://localhost：3000')
